6bf3d9097de89f204e2981cf336e58e595a02a8a
Dany Savard <dany.savard@gmail.com>
2019-02-10 14:36:43 -0400

    Config eslint. Config mocha. Document readme.md file.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+describe('Javascript Template Project', function() {
+	describe('# My First Test', function() {
+		it('should return true', function() {
+			assert.equal(true, true)
+		})
+	})
+})
