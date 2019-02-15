1006a9834e0436b02993729be95063b409b7fa98
Dany Savard <dany.savard@gmail.com>
2019-02-14 20:34:23 -0400

    Define env variable APP_MAINTENANCE.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const stage = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 3000
-const maintenance = process.env.MAINTENANCE != 'false'
+const maintenance = process.env.APP_MAINTENANCE != 'false'
 
 var app = express()
 
